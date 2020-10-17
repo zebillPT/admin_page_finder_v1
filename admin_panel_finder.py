7d42bb1d2afd5e751dd0ff8f52c38f3da19fd291
zebillPT <73021347+zebillPT@users.noreply.github.com>
2020-10-17 12:05:14 +0100

    Add files via upload

---
+++ admin_panel_finder.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+def Space(j):
+	i = 0
+	while i<=j:
+		print " ",
+		i+=1
+
+
+def findAdmin():
+	f = open("link.txt","r");
+	link = raw_input("Nome do Site \n(ex : site.com ou www.site.com ): ")
+	print "\n\nLinks Possiveis : \n"
+	while True:
+		sub_link = f.readline()
+		if not sub_link:
+			break
+		req_link = "http://"+link+"/"+sub_link
+		req = Request(req_link)
+		try:
+			response = urlopen(req)
+		except HTTPError as e:
+			continue
+		except URLError as e:
+			continue
+		else:
+			print "Encontrado => ",req_link
+
+def Credit():
+	Space(9); print "#####################################"
+	Space(9); print "#   +++ Admin Panel Finder v1 +++   #"
+	Space(9); print "#         Script by Zebill          #"
+	Space(9); print "#####################################"
+
+Credit()
+findAdmin()
